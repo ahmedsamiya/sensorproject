e0ce02035f5df68cc4d3d995da8f13ca827f5a1d
Samiya Ahmed <ahmedsamiya426@gmail.com>
2026-07-28 10:36:38 +0530

    executedpoject

--- src/exception.py
+++ src/exception.py
@@ -3,9 +3,9 @@ import sys
 def error_message_detail(error_message,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
 
-    filename=exc_tb.tb_frame.f_code.co_file_name
+    filename=exc_tb.tb_frame.f_code.co_filename
     error_message="Eror occured python script name [{0}] line number [{1}] error_message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
+        filename,exc_tb.tb_lineno,str(error_message)
     )
     return error_message
 
@@ -19,5 +19,5 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-def __str__(self):
-    return self.error_message
+    def __str__(self):
+        return self.error_message
